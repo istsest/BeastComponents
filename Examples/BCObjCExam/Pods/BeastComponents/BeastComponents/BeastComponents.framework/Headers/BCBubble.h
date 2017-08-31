d9c2e2224ff4b607ff3b60e33bc6b990690ceadb
Joon Jang <istsest@hotmail.com>
2017-08-31 16:08:49 -0700

    wip

---
+++ Examples/BCObjCExam/Pods/BeastComponents/BeastComponents/BeastComponents.framework/Headers/BCBubble.h
@@ -0,0 +1,19 @@
+//
+//  BCBubble.h
+//  BCBubble
+//
+//  Created by Joon Jang on 6/28/17.
+//  Copyright © 2017 Beast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BCBubble.
+FOUNDATION_EXPORT double BCBubbleVersionNumber;
+
+//! Project version string for BCBubble.
+FOUNDATION_EXPORT const unsigned char BCBubbleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BCBubble/PublicHeader.h>
+
+
